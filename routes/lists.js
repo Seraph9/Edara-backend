f458855bc39af3cb3879d85c39fe5a26b8f62f91
Abdullah Wafy <red.sulphur786@gmail.com>
2020-09-21 23:49:29 -0700

    minor change in put route handler for lists

--- routes/lists.js
+++ routes/lists.js
@@ -62,11 +62,11 @@ router.post('/', validateList, asyncHandler(async (req, res) => {
 //updates a list name, checks to see if user's id matches with "owner" of list
 //userId get passed in from the fetch on front end via local storage
 router.put('/:listId', asyncHandler(async (req, res, next) => {
-    const { name } = req.body; //userId should be passed into the request
+    const { title } = req.body; //userId should be passed into the request
     const listId = parseInt(req.params.listId, 10);
     const list = await List.findByPk(listId);
     //if (list && (Number(userId) === list.dataValues.userId)) {
-    await list.update({ name });
+    await list.update({ title });
     res.json({ list });
     // } else {
     //     next(userPermissionError());
